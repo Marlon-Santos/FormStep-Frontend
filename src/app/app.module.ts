ac4c6157290ff990a6c29e440062f8ebf87218ed
Marlon-Santos <marlonergondossantos@gmail.com>
2020-05-22 22:04:12 -0300

    add angular stepper

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatCardModule } from '@angular/material/card';
+import { SalvadosModule } from './salvados/salvados.module';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatCardModule,
-    MatButtonModule,
-    MatIconModule,
     BrowserAnimationsModule,
+    SalvadosModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
